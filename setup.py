fbea1091fc9eb163a00ef41e2bd97744051d932a
mrseyfi <seyfi.mehrdad@gmail.com>
2020-11-02 23:33:42 +0330

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from pathlib import Path
+setuptools.setup(
+    name="tsetmc_state",
+    author="Mehrdad Seyfi houjaghan",
+    version="1.2.1",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type='text/markdown',
+    description="A small web crawler for tsetmc.com",
+    packages=setuptools.find_packages(exclude=["tests","data"]),
+    project_urls={
+        'Source': 'https://github.com/mrseyfi/tsetmc_state',
+    },
+    install_requires=[
+          'requests',
+      ]
+)
