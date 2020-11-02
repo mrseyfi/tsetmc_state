5af90c805c8da86f0c525670e4e5bc342bde9fe8
mehrdad seyfi houjaghan <45231041+mrseyfi@users.noreply.github.com>
2020-11-02 22:23:58 +0330

    Create state.py

---
+++ state.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+from constants import BASE_URL
+class market:
+    def market_state(): 
+        html_data=rq.get(BASE_URL, timeout=5).text        
+
+        soup = BeautifulSoup(html_data, 'html.parser') #'html5lib' )
+        col = soup.find('tr').find_all('td')
+        if col and len(col)==2: return col[1].get_text()
+        else: return "نامشخص"
