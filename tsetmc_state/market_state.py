8240ad78f4f0aa52d6ec6bc8528c46b6d82eae71
mehrdad seyfi houjaghan <45231041+mrseyfi@users.noreply.github.com>
2020-12-07 11:52:11 +0330

    add try

--- tsetmc_state/market_state.py
+++ tsetmc_state/market_state.py
@@ -2,13 +2,16 @@ import requests as rq
 from bs4 import BeautifulSoup
 from tsetmc_state.constants import MAIN_URL
 class market:
-    def state(): 
-        html_data=rq.get(MAIN_URL, timeout=50).text        
+    def state():      
+        try:
+            html_data=rq.get(MAIN_URL, timeout=50).text        
 
-        soup = BeautifulSoup(html_data, 'html.parser') #'html5lib' )
-        col = soup.find('tr').find_all('td')
-        if col and len(col)==2: return col[1].get_text().strip()
-        else: return "نامشخص"
+            soup = BeautifulSoup(html_data, 'html.parser') #'html5lib' )
+            col = soup.find('tr').find_all('td')
+            if col and len(col)==2: print( col[1].get_text().strip())
+            else: return  "نامشخص"
+        except Exception as ex:
+            return  "خطا"
 
 #   by: mehrdad seyfi
 #   www.mrseyfi.ir
